458a0ff3829ac7b280a48a12795979aaa40006c6
Mateusz Gnat <wir03@Kamil-Mobicas-MacBook-Pro.local>
2018-04-06 01:39:32 +0200

    initial

---
+++ src/main/java/com/magn/magnspringboot/MagnSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.magn.magnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagnSpringBootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/magn/magnspringboot/MagnSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.magn.magnspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MagnSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
