426501b73717c399c0198c851f26bc5080b1ac04
Mateusz Gnat <mateusz.gnat@mobica.com>
2018-04-06 16:38:51 +0200

    first run with message

--- src/main/java/com/magn/magnspringboot/MagnSpringBootApplication.java
+++ src/main/java/com/magn/magnspringboot/MagnSpringBootApplication.java
@@ -2,6 +2,9 @@ package com.magn.magnspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MagnSpringBootApplication {
@@ -9,4 +12,38 @@ public class MagnSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MagnSpringBootApplication.class, args);
 	}
+
+
+	class Message {
+
+		private final String message;
+
+		public Message(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		@Override
+		public String toString() {
+			return "Message{" +
+					"message='" + message + '\'' +
+					'}';
+		}
+
+	}
+
+	@RestController
+	class Resources {
+
+		@RequestMapping( method = RequestMethod.GET)
+		public Message getMessage() {
+			return new Message("HOLAAAA");
+		}
+	}
+
+
+
 }
