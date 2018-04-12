4b88f524ba64a5545cf78b6f345e3db0dc63b20f
Mateusz Gnat <mateusz.gnat@mobica.com>
2018-04-12 16:25:25 +0200

    implement unittest for FakeDataDao

--- src/test/java/com/magn/magnspringboot/dao/FakeDataDaoTest.java
+++ src/test/java/com/magn/magnspringboot/dao/FakeDataDaoTest.java
@@ -5,7 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import static org.assertj.core.api.AssertionsForClassTypes.doesNotHave;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 
@@ -32,19 +35,60 @@ public class FakeDataDaoTest {
     }
 
     @Test
-    public void selectUserByUserUid() throws Exception {
+    public void shouldSelectUserByUserUid() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        User user = new User(uuid,"ana", "maniana", User.Gender.FEMAIL, 30, "ana.maniana@gmail.com");
+
+        fakeDataDao.insertUser(uuid, user);
+        assertThat(fakeDataDao.selectAllUsers()).hasSize(2);
+
+        Optional<User> userOptional = fakeDataDao.selectUserByUserUid(uuid);
+        assertThat(userOptional).isNotNull();
+        assertThat(userOptional.isPresent()).isTrue();
+        assertThat(user.getUserUid()).isEqualTo(uuid);
+        System.out.println(user.toString());
+    }
+
+    @Test
+    public void shouldNotSelectUserByUserUid() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        Optional<User> userFake = fakeDataDao.selectUserByUserUid(uuid);
+        assertThat(userFake.isPresent()).isFalse();
     }
 
     @Test
     public void updateUser() throws Exception {
+        UUID firstUserUid = fakeDataDao.selectAllUsers().get(0).getUserUid();
+        User newUser = new User(firstUserUid,"ana", "maniana", User.Gender.FEMAIL, 30, "ana.maniana@gmail.com");
+
+        fakeDataDao.updateUser(newUser);
+        Optional<User> userUpdate = fakeDataDao.selectUserByUserUid(firstUserUid);
+        assertThat(userUpdate.isPresent()).isTrue();
+        assertThat(fakeDataDao.selectAllUsers()).hasSize(1);
+        assertThat(fakeDataDao.selectUserByUserUid(firstUserUid).get().getFirstName()).isEqualTo("ana");
+        assertThat(userUpdate.get()).isEqualToComparingFieldByField(newUser);
+        System.out.println(userUpdate.toString());
+
     }
 
     @Test
     public void deleteUserByUserUid() throws Exception {
+        UUID userUid = fakeDataDao.selectAllUsers().get(0).getUserUid();
+        fakeDataDao.deleteUserByUserUid(userUid);
+        assertThat(fakeDataDao.selectAllUsers()).isEmpty();
+
     }
 
     @Test
     public void insertUser() throws Exception {
+
+        UUID userUid = UUID.randomUUID();
+        User user = new User(userUid,"joe", "manianana", User.Gender.MAIL, 30, "joe.manianana@gmail.com");
+        fakeDataDao.insertUser(userUid, user);
+
+        assertThat(fakeDataDao.selectUserByUserUid(userUid).get()).isEqualToComparingFieldByField(user);
+        System.out.println(fakeDataDao.selectUserByUserUid(userUid).toString());
+
     }
 
 }
---
+++ src/test/java/com/magn/magnspringboot/service/UserServiceTest.java
@@ -0,0 +1,36 @@
+package com.magn.magnspringboot.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by magn on 4/12/2018.
+ */
+public class UserServiceTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getAllUsers() throws Exception {
+    }
+
+    @Test
+    public void getUser() throws Exception {
+    }
+
+    @Test
+    public void updateUser() throws Exception {
+    }
+
+    @Test
+    public void removeUser() throws Exception {
+    }
+
+    @Test
+    public void insertUser() throws Exception {
+    }
+
+}
